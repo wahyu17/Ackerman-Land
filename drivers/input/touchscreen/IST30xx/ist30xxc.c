34ef210e8704b6d0d2e5fc23beeaf02f5349fd4f
nichcream <c.potato23@gmail.com>
2018-04-17 23:43:13 +0800

    input: touchscreen: Switch to a simple separate suspend state variable
    There may be a chance that our custom struct does not get updated as
    it's only being set at probe.
    
    This will also allow us to report suspend state to WG only when
    touchscreen is set up for it.

--- drivers/input/touchscreen/IST30xx/ist30xxc.c
+++ drivers/input/touchscreen/IST30xx/ist30xxc.c
@@ -115,6 +115,8 @@ int fhd_key_dim_x[] = { 0, FHD_MENU_KEY_X, FHD_HOME_KEY_X, FHD_BACK_KEY_X, };
 struct ist30xx_data *ts_data;
 struct ist30xx_data *global_ts_data;
 
+static bool ts_suspended = false;
+
 #if CTP_CHARGER_DETECT
 extern int power_supply_get_battery_charge_state(struct power_supply *psy);
 static struct power_supply *batt_psy;
@@ -201,9 +203,8 @@ void ist30xx_enable_irq(struct ist30xx_data *data)
 }
 
 #ifdef CONFIG_WAKE_GESTURES
-struct ist30xx_data *ist30xx_ts = NULL;
 bool scr_suspended_ist(void) {
-	return ist30xx_ts->suspended;
+	return ts_suspended;
 }
 #endif
 
@@ -1298,7 +1299,7 @@ static int ist30xx_suspend(struct device *dev)
 			ev_btn_status = true;
 		}
 		ist30xx_irq_handler(data->client->irq, true);
-		data->suspended = true;
+		ts_suspended = true;
 
 		if (dt2w_switch_changed) {
 			dt2w_switch = dt2w_switch_temp;
@@ -1333,9 +1334,6 @@ static int ist30xx_suspend(struct device *dev)
 #endif
 	mutex_unlock(&ist30xx_mutex);
 
-#ifdef CONFIG_WAKE_GESTURES
-	data->suspended = true;
-#endif
 	return 0;
 }
 
@@ -1358,7 +1356,7 @@ static int ist30xx_resume(struct device *dev)
 		}
 
 		ist30xx_irq_handler(data->client->irq, false);
-		data->suspended = false;
+		ts_suspended = false;
 
 		return 0;
 	}
@@ -1391,9 +1389,6 @@ static int ist30xx_resume(struct device *dev)
 			      CHECK_CHARGER_INTERVAL);
 #endif
 
-#ifdef CONFIG_WAKE_GESTURES
-	data->suspended = false;
-#endif
 	return 0;
 }
 
@@ -2141,7 +2136,6 @@ static int ist30xx_probe(struct i2c_client *client,
 		goto err_init_drv;
 
 #ifdef CONFIG_WAKE_GESTURES
-	ist30xx_ts = data;
 	device_init_wakeup(&client->dev, 1);
 #endif
 
@@ -2198,9 +2192,6 @@ static int ist30xx_probe(struct i2c_client *client,
 #if IST30XX_GESTURE
 	data->suspend = false;
 	data->gesture = false;
-#endif
-#ifdef CONFIG_WAKE_GESTURES
-	data->suspended = false;
 #endif
 	data->ignore_delay = false;
 	data->secure_mode = false;
